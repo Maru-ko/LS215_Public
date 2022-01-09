badc6505a73696b61c444a3eed156be32459f889
Marcos <53981643+Maru-ko@users.noreply.github.com>
2022-01-09 01:49:45 -0500

    天使なのに産まれちゃってさ

--- Lesson3/VersionComparison1.js
+++ Lesson3/VersionComparison1.js
@@ -1,30 +1,29 @@
 const gClause = str => /[^0-9.]/.test(str);
-
-const dotCheck = str => str.match(/\./g) >= str.match(/\d/g);
+const dotCheck = str => str.match(/\./g)?.length >= str.match(/\d/g)?.length;
 
 function compareVersions(v1, v2) {
   if (gClause(v1) || gClause(v2)) return null;    
   if (dotCheck(v1) || dotCheck(v2)) return null;
-
-  let arrV1 = v1.split('.');
-  let arrV2 = v2.split('.');
-  
+  let max = Math.max(v1.split('.').length, v2.split('.').length);  
+  let arrV1 = padder(v1, max).split('.');
+  let arrV2 = padder(v2, max).split('.');
   let v1Count = 0;
   let v2Count = 0;
-  
-  let rounds = Math.min(arrV1.length, arrV2.length);
-  
-  for (let i = 0; i < rounds; ++i) {
+
+  for (let i = 0; i < max; ++i) {
     let x = arrV1[i];
     let y = arrV2[i];
-   if (x > y) {
-     v1Count += 1;
-   } else if (x < y) {
-     v2Count += 2;
-   }
+   x > y ? v1Count += 1 : x < y ? v2Count += 1 : 'hi';
   }
   
   return v1Count > v2Count ? 1 : v1Count < v2Count ? -1 : 0;
+}
+
+
+function padder(str, max) {
+  let rounds = max - (str.split('.').length);
+  for (let i = 0; i < rounds; ++i) str += '.0'
+  return str;
 }
 
 
@@ -35,3 +34,17 @@ console.log(compareVersions(a, b));
 console.log(compareVersions('1.18.2', '13.37'));
 console.log(compareVersions('0.1', '1'));
 console.log(compareVersions('1', '1.0'));
+
+console.log(compareVersions('1', '1'));            // 0
+console.log(compareVersions('1.1', '1.0'));        // 1
+console.log(compareVersions('2.3.4', '2.3.5'));    // -1
+console.log(compareVersions('1.a', '1'));          // null
+console.log(compareVersions('.1', '1'));           // null
+console.log(compareVersions('1.', '2'));           // null
+console.log(compareVersions('1..0', '2.0'));       // null
+console.log(compareVersions('1.0', '1.0.0'));      // 0
+console.log(compareVersions('1.0.0', '1.1'));      // -1
+console.log(compareVersions('1.0', '1.0.5'));      // -1
+ 
+console.log(padder('13.37', 3));
+
